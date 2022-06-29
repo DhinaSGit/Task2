bb8412ed9dc7e38d353ad341bdb6427e4b309415
DhinaSGit <dhinakaran7595@gmail.com>
2022-06-29 10:09:51 +0530

    task2

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,17 @@
+package org.add;
+
+public class GreensTech {
+
+	public void greensOmr(String l1, String l2, String l3, String l4) {
+		
+		System.out.println("Loaction 1"+'\t'+":"+l1+'\n'+"Location 2"+'\t'+":"+l2+'\n'+"Location 3"+'\t'+":"+l3+'\n'+"Location 4"+'\t'+":"+l4);
+		
+	}
+	public static void main(String[] args) {
+		
+		GreensTech g = new GreensTech();
+		
+		g.greensOmr("OMR", "Tambaram", "Anna Nagar", "Velacherry");
+		
+	}
+}
